7c3d6b6b6103bdcaed45d853a4c52dfb906233fd
zhm <954522398@qq.com>
2026-07-29 11:54:44 +0800

    Deploy Mario Growth Island

---
+++ sw.js
@@ -0,0 +1,18 @@
+const CACHE = "mario-growth-island-v2";
+const CORE = ["/", "/manifest.webmanifest", "/icon-192.png", "/icon-512.png"];
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(CORE)));
+  self.skipWaiting();
+});
+self.addEventListener("activate", (event) => {
+  event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key)))));
+  self.clients.claim();
+});
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+  event.respondWith(fetch(event.request).then((response) => {
+    const copy = response.clone();
+    caches.open(CACHE).then((cache) => cache.put(event.request, copy));
+    return response;
+  }).catch(() => caches.match(event.request).then((cached) => cached || caches.match("/"))));
+});
